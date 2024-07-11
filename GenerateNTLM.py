5c4bb4ae22d3753e1d04a0f8279787b78d6a618e
AndreyF2018 <and.fed99@yandex.ru>
2024-07-12 00:55:46 +0400

    Init com

---
+++ GenerateNTLM.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:962d39a287ac4067ecff13d0c0f4fcb496959aae01448637e38ce3cec82ecc64
+size 608
---
+++ PasswordsMonitoring.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b36f3554c0b65479008300fd9cd49cbf107b4c712240e61793cd60cd66f48cbd
+size 5966
